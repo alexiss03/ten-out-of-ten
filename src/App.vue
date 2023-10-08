04e6deded53f84136f3456dae3c9312a84e0ade0
Mary Alexis Solis <>
2023-10-08 10:04:27 +0800

    - added section 3 and section 4, reformat first section size

--- src/App.vue
+++ src/App.vue
@@ -10,9 +10,21 @@ import { RouterLink, RouterView } from 'vue-router'
             <img src="@/assets/images/logo.png" id="logo-image">
         </div>
         <div class="section2"> 
+          FIRST CHAPTER<br>
           Our first collection has arrived
-          women clothing
+          
         </div>
+        <div class="section3"> 
+          <div class="section3-box">
+          </div>
+          <div>
+          </div>
+          <div>
+          </div>
+        </div>
+        <div class="section4"> 
+          exceptional quality in terms of the stylish modern trendy design mixing with vintage timeless colors resulting to effortless sophistication
+        </div>      
       </div>
     </body>
   </html>
@@ -26,7 +38,7 @@ body, html {
   margin: 0;
   width: 100%;
   min-height: 100%;
-  font-family: 'JosefinSans-Regular';
+  font-family: 'Coves-Light';
 
 }
 
@@ -36,11 +48,21 @@ body {
 
 .section1 {
   background: #FFFFFF;
-  height: 200px;
+  height: 180px;
   width: 80vw;
   margin: 0px;
   padding: 0px;
   margin-top: 100px;
+  display: inline-block;
+  vertical-align: middle;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+}
+
+#logo-image {
+  margin: auto;
+  width: 30%;
 }
 
 .section2 {
@@ -53,14 +75,45 @@ body {
   padding-top: 300px;
   text-align: center;
   font-size: 25px;
+  color: white;
+  background-image: url("@/assets/images/paris.png")
 }
 
-#logo-image {
-  width: 30%;
-  position: absolute;
-  left: 35%;
-  top: 20%;
+.section3 {
+  background: #FFFFFF;
+  height: 400px;
+  width: 80vw;
+  margin: 0px;
+  padding: 0px;
+  float: left;
+  padding-top: 300px;
+  text-align: center;
+  font-size: 25px;
   
+}
+
+.section3-box {
+  background: #FFFFFF;
+  height: 400px;
+  width: 80vw;
+  margin: 0px;
+  padding: 0px;
+  float: left;
+  padding-top: 300px;
+  text-align: center;
+  font-size: 25px;
+}
+
+.section4 { 
+  background: #65451F;
+  height: 400px;
+  width: 80vw;
+  margin: 0px;
+  padding: 0px;
+  float: left;
+  padding-top: 300px;
+  text-align: center;
+  font-size: 25px;
 }
 
 </style>
